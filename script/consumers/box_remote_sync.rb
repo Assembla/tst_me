b8dfeaed72bf33599b3b41533878a3fedfe0da3d
Alexandru C <alexandru@assembla.com>
2015-03-23 14:11:52 +0200

    cleanup consumer scripts a little.

--- script/consumers/box_remote_sync.rb
+++ script/consumers/box_remote_sync.rb
@@ -2,31 +2,8 @@
 # encoding: utf-8
 
 $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/../../lib/")
-require "configuration"
-require "mq_connection"
-require "processors/box_remote_sync_processor"
+require 'consumer'
 
 puts "Starting Box Remote Sync Consumer"
-begin
-  processor = BoxRemoteSyncProcessor.new
-  mq_connection = MQConnection.new
-  channel = mq_connection.channel
-  loop do
-    begin
-      mq_connection.subscribe("box_remote_sync", {:manual_ack => true, :routing_key => "box_connector.sync.*.requested"}) do |delivery_info, payload|
-        payload = HashWithIndifferentAccess.new(JSON.parse(payload))
-        processor.metadata = delivery_info
-        processor.on_message(payload)
-        channel.acknowledge(delivery_info.delivery_tag)
-      end
-    rescue => ex
-      puts "Error:\n #{ex}"
-      sleep(3)
-    end
-  end
-rescue Interrupt
-  puts "Consumer stopped."
-  exit(0)
-ensure
-  mq_connection.connection.close
-end
+
+Consumer.run({queue_name: "box_remote_sync", routing_key: "box_connector.sync.*.requested", processor: "box_remote_sync_processor"})
--- script/consumers/box_s3_upload.rb
+++ script/consumers/box_s3_upload.rb
@@ -2,31 +2,8 @@
 # encoding: utf-8
 
 $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/../../lib/")
-require "configuration"
-require "mq_connection"
-require "processors/box_s3_upload_processor"
+require 'consumer'
 
 puts "Started Box S3 Upload Consumer"
-begin
-  processor = BoxS3UploadProcessor.new
-  mq_connection = MQConnection.new
-  channel = mq_connection.channel
-  loop do
-    begin
-      mq_connection.subscribe("box_s3_sync", {:manual_ack => true, :routing_key => "box_connector.sync.s3.requested"}) do |delivery_info, payload|
-        payload = HashWithIndifferentAccess.new(JSON.parse(payload))
-        processor.metadata = delivery_info
-        processor.on_message(payload)
-        channel.acknowledge(delivery_info.delivery_tag)
-      end
-    rescue => ex
-      puts "Error:\n #{ex}"
-      sleep(3) #wait some time before retrying to subscribe
-    end
-  end
-rescue Interrupt
-  puts "Consumer stopped."
-  exit(0)
-ensure
-  mq_connection.connection.close
-end
+
+Consumer.run({queue_name: "box_s3_sync", routing_key: "box_connector.sync.s3.requested", processor: "box_s3_upload_processor"})
